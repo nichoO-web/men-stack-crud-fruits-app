bb68eef92a1191ded721d03dad126524bba1c043
nichoOtero <nichonaks@gmail.com>
2024-10-07 14:52:08 -0500

    create new fruits

--- server.js
+++ server.js
@@ -14,8 +14,28 @@ mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 });
 
+const Fruit = require('./models/fruit');
+
+// lets add our middleware
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
     res.render('index.ejs');
+});
+
+app.get('/fruits/new', (req, res) => {
+    res.render('fruits/new.ejs');
+});
+
+app.post('/fruits', async (req, res) => {
+    if (req.body.isReadyToEat === 'on') {
+        req.body.isReadyToEat = true;
+    } else {
+        req.body.isReadyToEat = false;
+    }
+
+    await Fruit.create(req.body);
+    res.redirect('/fruits/new');
 });
 
 app.listen(3000, () => {
